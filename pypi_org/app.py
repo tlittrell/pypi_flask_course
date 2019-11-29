5f027c4f4ebae89573049049cce4cb579b2dec35
tlittrell <littrell.thomas@gmail.com>
2019-11-29 14:15:39 -0500

    Add initial index and pre-commit

--- pypi_org/app.py
+++ pypi_org/app.py
@@ -1,9 +1,13 @@
 import flask
 
-app = flask.Flask('pypi_org')
+app = flask.Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def hello_world():
-    return "Hello world."
+    test_packages = ["package1", "package2", "package3"]
+    return flask.render_template("index.html", packages=test_packages)
 
-app.run()
+
+if __name__ == "__main__":
+    app.run(debug=True)
